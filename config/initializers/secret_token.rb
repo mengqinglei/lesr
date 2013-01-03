26d29bd12a1070c4d57bbc206948c7e0101f4d22
Ben Zhang <ben@Bens-MacBook-Pro.local>
2013-01-03 18:50:11 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lesr::Application.config.secret_token = 'f65d458bf4c237121db048441d7a582dfd66fdc7ed75ab9672607668f4237a4f84de5991ffe3ebf88b0b07b467a92a3e76f2c1e74ac6c23801d8422f12081008'
